63d7908cb6c3a62bebf549d660460327e1c236df
NBriggsy <hello.briggs1@gmail.com>
2017-11-13 01:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma (string){
   var lowercase = 'hello!'
      lowercase.toLowerCase() === lowercase // true
      return "I can't hear you!"
-  var uppercase = "HELLO!"
-     uppercase.toUpperCase() === uppercase // true
+  var lowercase = "HELLO!"
+     lowercase.toUpperCase() === uppercase // true
      return "YES INDEED!"
 }
